5440012ee0a6d6ff3edcb8a663689625516d8045
CircuitSyn <circuitsyn@gmail.com>
2018-09-05 21:00:43 -0700

    fixed multiple defenders issue

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -21,6 +21,7 @@ var gameStatus = {
     defenderInPlace: false,
     yourCharacter: '',
     defKillCount: 3,
+    gameOn: false, //boolean key to track single defender in game location
 };
 
 var pickleRick = {
@@ -82,100 +83,117 @@ var morty = {
 console.log(gameStatus.stage);
 //character one behavior flow
 $("#char1").click(function() {
-    
-    if (gameStatus.stage == 0){
-        gameStatus.stage++;
-        console.log(gameStatus.stage);
-        $("#yourChar").append($("#char1"));
-        yourCharacter = pickleRick;
-        pickleRick.attkHold = pickleRick.attackPwr;
-        $("#enemiesAvail").append($("#char2"));
-        $("#enemiesAvail").append($("#char3"));
-        $("#enemiesAvail").append($("#char4"));
-    }
-    // console.log(gameStatus.stage);
-    else if (gameStatus.stage == 1) {
-        $("#defender").append($("#char1"));
-        defenderInPlace = true;
-        // defenderChar = pickleRick;
-        defenderChar = Object.assign({},pickleRick);
-    }
+    if (gameStatus.gameOn != true){
 
+
+        if (gameStatus.stage == 0){
+            gameStatus.stage++;
+            console.log(gameStatus.stage);
+            $("#yourChar").append($("#char1"));
+            yourCharacter = pickleRick;
+            pickleRick.attkHold = pickleRick.attackPwr;
+            $("#enemiesAvail").append($("#char2"));
+            $("#enemiesAvail").append($("#char3"));
+            $("#enemiesAvail").append($("#char4"));
+        }
+        // console.log(gameStatus.stage);
+        else if (gameStatus.stage == 1) {
+            $("#defender").append($("#char1"));
+            defenderInPlace = true;
+            gameStatus.gameOn = true;
+            // defenderChar = pickleRick;
+            defenderChar = Object.assign({},pickleRick);
+        }
+    }
+    else {
+        
+    }
   });
 
 //character two behavior flow
 $("#char2").click(function() {
-    
-    if (gameStatus.stage == 0){
-        gameStatus.stage++;
-        $("#yourChar").append($("#char2"));
-        yourCharacter = mrMeeSeeks;
-        mrMeeSeeks.attkHold = mrMeeSeeks.attackPwr;
-        $("#enemiesAvail").append($("#char1"));
-        $("#enemiesAvail").append($("#char3"));
-        $("#enemiesAvail").append($("#char4"));
+    if (gameStatus.gameOn != true){    
+        if (gameStatus.stage == 0){
+            gameStatus.stage++;
+            $("#yourChar").append($("#char2"));
+            yourCharacter = mrMeeSeeks;
+            mrMeeSeeks.attkHold = mrMeeSeeks.attackPwr;
+            $("#enemiesAvail").append($("#char1"));
+            $("#enemiesAvail").append($("#char3"));
+            $("#enemiesAvail").append($("#char4"));
+        }
+        
+        else if (gameStatus.stage == 1) {
+            $("#defender").append($("#char2"));
+            defenderInPlace = true;
+            gameStatus.gameOn = true;
+            // defenderChar = mrMeeSeeks;
+            defenderChar = Object.assign({},mrMeeSeeks);
+        }
     }
-    
-    else if (gameStatus.stage == 1) {
-        $("#defender").append($("#char2"));
-        defenderInPlace = true;
-        // defenderChar = mrMeeSeeks;
-        defenderChar = Object.assign({},mrMeeSeeks);
-    }
+    else {
 
+    }
   });
 
 //character three behavior flow
 $("#char3").click(function() {
-    
-    if (gameStatus.stage == 0){
-        gameStatus.stage++;
-        $("#yourChar").append($("#char3"));
-        yourCharacter = scaryTerry;
-        scaryTerry.attkHold = scaryTerry.attackPwr;
-        $("#enemiesAvail").append($("#char1"));
-        $("#enemiesAvail").append($("#char2"));
-        $("#enemiesAvail").append($("#char4"));
+    if (gameStatus.gameOn != true){
+        if (gameStatus.stage == 0){
+            gameStatus.stage++;
+            $("#yourChar").append($("#char3"));
+            yourCharacter = scaryTerry;
+            scaryTerry.attkHold = scaryTerry.attackPwr;
+            $("#enemiesAvail").append($("#char1"));
+            $("#enemiesAvail").append($("#char2"));
+            $("#enemiesAvail").append($("#char4"));
+        }
+        
+        else if (gameStatus.stage == 1) {
+            $("#defender").append($("#char3"));
+            defenderInPlace = true;
+            gameStatus.gameOn = true;
+            // defenderChar = scaryTerry;
+            defenderChar = Object.assign({},scaryTerry);
+            console.log(defenderChar);
+        }
     }
-    
-    else if (gameStatus.stage == 1) {
-        $("#defender").append($("#char3"));
-        defenderInPlace = true;
-        // defenderChar = scaryTerry;
-        defenderChar = Object.assign({},scaryTerry);
-        console.log(defenderChar);
-    }
+    else {
 
+    }
   });
 
 //character four behavior flow
 $("#char4").click(function() {
-    
-    if (gameStatus.stage == 0){
-        gameStatus.stage++;
-        $("#yourChar").append($("#char4"));
-        yourCharacter = morty;
-        morty.attkHold = morty.attackPwr;
-        $("#enemiesAvail").append($("#char1"));
-        $("#enemiesAvail").append($("#char2"));
-        $("#enemiesAvail").append($("#char3"));
-    }
-    
-    else if (gameStatus.stage == 1) {
-        $("#defender").append($("#char4"));
-        defenderInPlace = true;
-        // defenderChar = morty;
-        defenderChar = Object.assign({},morty);
-
-
-    }
-
-    else if ((gameStatus.stage == 2) && (dead == false)) {
-
+    if (gameStatus.gameOn != true){
+        if (gameStatus.stage == 0){
+            gameStatus.stage++;
+            $("#yourChar").append($("#char4"));
+            yourCharacter = morty;
+            morty.attkHold = morty.attackPwr;
+            $("#enemiesAvail").append($("#char1"));
+            $("#enemiesAvail").append($("#char2"));
+            $("#enemiesAvail").append($("#char3"));
+        }
+        
+        else if (gameStatus.stage == 1) {
+            $("#defender").append($("#char4"));
+            defenderInPlace = true;
+            gameStatus.gameOn = true;
+            // defenderChar = morty;
+            defenderChar = Object.assign({},morty);
+        }
     }
     else {
-        return;
+
     }
+
+        // else if ((gameStatus.stage == 2) && (dead == false)) {
+
+        // }
+        // else {
+        //     return;
+        // }
 
   });
 
@@ -209,6 +227,7 @@ $("#char4").click(function() {
         else if ((defenderChar.health <= 0)&&(gameStatus.defKillCount <= 0)) {
             alert("You've won the game! Click Restart to try and win again!");
             $("#restart").addClass("restartButtonShow");
+            gameStatus.gameOn = false;
         }
 
         //calculate bar decrease ratio for progress bars
